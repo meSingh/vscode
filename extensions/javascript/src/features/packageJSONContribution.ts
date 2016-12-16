meSingh/vscode

commit c37233e7920455efbafb27e0ff084f2c41fd745f

migrate [bower|package]JSONContributions, #15573

extensions/javascript/src/features/packageJSONContribution.ts
@@ -65,11 +65,11 @@ export class PackageJSONContribution implements IJSONContribution {
 									let keys = results[i].key;
 									if (Array.isArray(keys) && keys.length > 0) {
 										let name = keys[0];
-										let insertText = JSON.stringify(name);
+										let insertText = new SnippetString().appendText(JSON.stringify(name));
 										if (addValue) {
-											insertText += ': "{{*}}"';
+											insertText.appendText(': ').appendPlaceholder('*');
 											if (!isLast) {
-												insertText += ',';
+												insertText.appendText(',');
 											}
 										}
 										let proposal = new CompletionItem(name);
@@ -97,11 +97,11 @@ export class PackageJSONContribution implements IJSONContribution {
 				});
 			} else {
 				this.mostDependedOn.forEach((name) => {
-					let insertText = JSON.stringify(name);
+					let insertText = new SnippetString().appendText(JSON.stringify(name));
 					if (addValue) {
-						insertText += ': "{{*}}"';
+						insertText.appendText(': ').appendPlaceholder('*');
 						if (!isLast) {
-							insertText += ',';
+							insertText.appendText(',');
 						}
 					}
 					let proposal = new CompletionItem(name);

extensions/javascript/src/features/bowerJSONContribution.ts
@@ -62,11 +62,11 @@ export class BowerJSONContribution implements IJSONContribution {
 								for (let i = 0; i < results.length; i++) {
 									let name = results[i].name;
 									let description = results[i].description || '';
-									let insertText = JSON.stringify(name);
+									let insertText = new SnippetString().appendText(JSON.stringify(name));
 									if (addValue) {
-										insertText += ': "{{latest}}"';
+										insertText.appendText(': ').appendPlaceholder('latest');
 										if (!isLast) {
-											insertText += ',';
+											insertText.appendText(',');
 										}
 									}
 									let proposal = new CompletionItem(name);
@@ -91,11 +91,11 @@ export class BowerJSONContribution implements IJSONContribution {
 				});
 			} else {
 				this.topRanked.forEach((name) => {
-					let insertText = JSON.stringify(name);
+					let insertText = new SnippetString().appendText(JSON.stringify(name));
 					if (addValue) {
-						insertText += ': "{{latest}}"';
+						insertText.appendText(': ').appendPlaceholder('latest');
 						if (!isLast) {
-							insertText += ',';
+							insertText.appendText(',');
 						}
 					}
 

commit fbff4f89e4d3ff7cab801195580c5714bb742910

Fix package.json Auto complete
Fixes #17342

**Bug**
I believe that c37233e7920455efbafb27e0ff084f2c41fd745f introduced a regression for package.json auto complete where we end up inserting the text `*` for the package.version

**Fix**
Insert `""` instead of star to match previous behavior better

extensions/javascript/src/features/packageJSONContribution.ts
@@ -67,7 +67,7 @@ export class PackageJSONContribution implements IJSONContribution {
 										let name = keys[0];
 										let insertText = new SnippetString().appendText(JSON.stringify(name));
 										if (addValue) {
-											insertText.appendText(': ').appendPlaceholder('*');
+											insertText.appendText(': "').appendPlaceholder('').appendText('"');
 											if (!isLast) {
 												insertText.appendText(',');
 											}
@@ -99,7 +99,7 @@ export class PackageJSONContribution implements IJSONContribution {
 				this.mostDependedOn.forEach((name) => {
 					let insertText = new SnippetString().appendText(JSON.stringify(name));
 					if (addValue) {
-						insertText.appendText(': ').appendPlaceholder('*');
+						insertText.appendText(': "').appendPlaceholder('').appendText('"');
 						if (!isLast) {
 							insertText.appendText(',');
 						}