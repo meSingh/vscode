meSingh/vscode

commit c1c6d713ad3e4a6090f9054a34df5777a8214c3e

Fix middle mouse button opening broken release notes link in browser

src/vs/workbench/contrib/webview/browser/pre/main.js
@@ -188,6 +188,11 @@
 				return;
 			}
 
+			// Prevent middle clicks opening a broken link in the browser
+			if (event.button == 1) {
+				event.preventDefault();
+			}
+
 			let baseElement = event.view.document.getElementsByTagName('base')[0];
 			/** @type {any} */
 			let node = event.target;
@@ -443,7 +448,7 @@
 					});
 
 					// Bubble out link clicks
-					newFrame.contentWindow.addEventListener('click', handleInnerClick);
+					newFrame.contentWindow.addEventListener('mousedown', handleInnerClick);
 
 					if (host.onIframeLoaded) {
 						host.onIframeLoaded(newFrame);

commit 44b8253a8095fd8ed950712444b4ed713d477e6a

Use auxclick and click events to intercept clicks in webviews
c1c6d713ad3e4a6090f9054a34df5777a8214c3e introduced a regression by changing to use `mousedown` instead of `click`. This means that we were not properly able to call `preventDefault` to block the default link open behavior.

The fix is to use `click` for intercepting clicks plus `auxclick` for blocking middle clicks

src/vs/workbench/contrib/webview/browser/pre/main.js
@@ -188,11 +188,6 @@
 				return;
 			}
 
-			// Prevent middle clicks opening a broken link in the browser
-			if (event.button == 1) {
-				event.preventDefault();
-			}
-
 			let baseElement = event.view.document.getElementsByTagName('base')[0];
 			/** @type {any} */
 			let node = event.target;
@@ -212,6 +207,27 @@
 					break;
 				}
 				node = node.parentNode;
+			}
+		};
+
+		/**
+		 * @param {MouseEvent} event
+		 */
+		const handleAuxClick = (event) => {
+			// Prevent middle clicks opening a broken link in the browser
+			if (!event.view || !event.view.document) {
+				return;
+			}
+
+			if (event.button === 1) {
+				let node = /** @type {any} */ (event.target);
+				while (node) {
+					if (node.tagName && node.tagName.toLowerCase() === 'a' && node.href) {
+						event.preventDefault();
+						break;
+					}
+					node = node.parentNode;
+				}
 			}
 		};
 
@@ -459,7 +475,8 @@
 					});
 
 					// Bubble out link clicks
-					newFrame.contentWindow.addEventListener('mousedown', handleInnerClick);
+					newFrame.contentWindow.addEventListener('click', handleInnerClick);
+					newFrame.contentWindow.addEventListener('auxclick', handleAuxClick);
 
 					if (host.onIframeLoaded) {
 						host.onIframeLoaded(newFrame);