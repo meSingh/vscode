meSingh/vscode

commit 954ea146bebd4446258233aabf66b1e05b13e651

Fixes `Terminal: Focus Terminal Tabs view` command not working (#164408)

src/vs/workbench/contrib/terminal/browser/terminalGroupService.ts
@@ -179,14 +179,7 @@ export class TerminalGroupService extends Disposable implements ITerminalGroupSe
 				if (pane && !pane.isVisible()) {
 					await this._viewsService.openView(TERMINAL_VIEW_ID, focus);
 				}
-				// Do not await the focus as setVisible must be called immediately to ensure
-				// something else didn't steal focus
-				instance.focusWhenReady(true);
-				// HACK: as a workaround for https://github.com/microsoft/vscode/issues/134692,
-				// this will trigger a forced refresh of the viewport to sync the viewport and scroll bar.
-				// This can likely be removed after https://github.com/xtermjs/xterm.js/issues/291 is
-				// fixed upstream.
-				instance.setVisible(true);
+				await instance.focusWhenReady(true);
 			}
 		}
 		this._onDidShow.fire();

src/vs/workbench/contrib/terminal/browser/terminalView.ts
@@ -47,7 +47,6 @@ import { ServicesAccessor } from 'vs/editor/browser/editorExtensions';
 import { TerminalCapability } from 'vs/platform/terminal/common/capabilities/capabilities';
 
 export class TerminalViewPane extends ViewPane {
-	private _actions: IAction[] | undefined;
 	private _fontStyleElement: HTMLElement | undefined;
 	private _parentDomElement: HTMLElement | undefined;
 	private _terminalTabbedView?: TerminalTabbedView;
@@ -80,11 +79,6 @@ export class TerminalViewPane extends ViewPane {
 	) {
 		super(options, keybindingService, _contextMenuService, _configurationService, _contextKeyService, viewDescriptorService, _instantiationService, openerService, themeService, telemetryService);
 		this._register(this._terminalService.onDidRegisterProcessSupport(() => {
-			if (this._actions) {
-				for (const action of this._actions) {
-					action.enabled = true;
-				}
-			}
 			this._onDidChangeViewWelcomeState.fire();
 		}));
 

commit 31121e48d33bc472e919a6b939c4afe47fe281b7

Bring back hack to refresh terminal scroll bar
Regressed in #164408
Fixes #174380

src/vs/workbench/contrib/terminal/browser/terminalGroupService.ts
@@ -188,6 +188,11 @@ export class TerminalGroupService extends Disposable implements ITerminalGroupSe
 				if (pane && !pane.isVisible()) {
 					await this._viewsService.openView(TERMINAL_VIEW_ID, focus);
 				}
+				// HACK: as a workaround for https://github.com/microsoft/vscode/issues/134692,
+				// this will trigger a forced refresh of the viewport to sync the viewport and scroll bar.
+				// This can likely be removed after https://github.com/xtermjs/xterm.js/issues/291 is
+				// fixed upstream.
+				instance.setVisible(true);
 				await instance.focusWhenReady(true);
 			}
 		}